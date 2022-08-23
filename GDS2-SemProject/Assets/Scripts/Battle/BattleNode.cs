6118023689f02a5847f14c56fb780c3d860a3b22
Richard.K <0rickcir0@gmail.com>
2022-08-23 22:36:01 +1000

    TeamChange
    Buildings can change teams

--- GDS2-SemProject/Assets/Scripts/Battle/BattleNode.cs
+++ GDS2-SemProject/Assets/Scripts/Battle/BattleNode.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class BattleNode : MonoBehaviour
 {
+    private SpriteRenderer sr;
     [SerializeField] private BattleNode[] neighbourNodes;
     private Transform[] neighbourNodeTransform;
     [SerializeField] private LineController line;
-    // Start is called before the first frame update
+
+    // Castle Stats
+    [SerializeField] private float castleMaxHP = 10;
+    [SerializeField] private float castleCurrHP;
+
+    [SerializeField] private bool isEnemy = true;
+    [SerializeField] private Unit[] summonedUnits;
+
+
     void Start()
     {
+        sr = GetComponent<SpriteRenderer>();
+        castleCurrHP = castleMaxHP;
         LineUpdate();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(castleCurrHP <= 0)
+        {
+            CastleCapture();
+        }
+
+        //castleCurrHP -= 1 * Time.deltaTime;
     }
     
     private void LineUpdate()
@@ -28,4 +45,12 @@ public class BattleNode : MonoBehaviour
        }
        line.SetUpLine(neighbourNodeTransform);
     }
+
+    private void CastleCapture()
+    {
+        isEnemy = !isEnemy;
+        sr.color = isEnemy ? Color.red : Color.blue; 
+        castleCurrHP = castleMaxHP;
+    }
+
 }
