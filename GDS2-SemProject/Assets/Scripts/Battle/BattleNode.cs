ae40dc23ec0132da3cf091d8f539736ac80dd496
Richard.K <0rickcir0@gmail.com>
2022-08-28 22:34:12 +1000

    Lot of progress
    - Path creation complete
    - Unit spawn for temp enemies complete
    - Nodes generally work

--- GDS2-SemProject/Assets/Scripts/Battle/BattleNode.cs
+++ GDS2-SemProject/Assets/Scripts/Battle/BattleNode.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class BattleNode : MonoBehaviour
 {
     private SpriteRenderer sr;
-    [SerializeField] private BattleNode[] neighbourNodes;
-    private Transform[] neighbourNodeTransform;
-    [SerializeField] private LineController line;
+    [SerializeField] private List<BattleNode> neighbourNodes;
+
+    //[SerializeField] private LineController line;
 
     // Castle Stats
     [SerializeField] private float castleMaxHP = 10;
@@ -16,18 +16,38 @@ public class BattleNode : MonoBehaviour
     [SerializeField] private bool isEnemy = true;
     [SerializeField] private Unit[] summonedUnits;
 
+    [SerializeField] private Unit testUnit;
 
-    void Start()
-    {
+    [SerializeField] private GameObject path;
+    //[SerializeField] private GameObject pathParent;
+
+    [SerializeField] private bool isBoss;
+
+    private int splitCount = 1;
+
+    void Awake()
+    { 
         sr = GetComponent<SpriteRenderer>();
         castleCurrHP = castleMaxHP;
-        LineUpdate();
+        CreatePaths();
+        foreach(BattleNode i in neighbourNodes)
+        {
+            i.AddNeighbour(this);
+        }
+    }
 
+    private void Start()
+    {
+        if (isEnemy)
+        {
+            EnemySummonUnits();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        //If the castle has been 'taken over', switch sides.
         if(castleCurrHP <= 0)
         {
             CastleCapture();
@@ -36,21 +56,121 @@ public class BattleNode : MonoBehaviour
         //castleCurrHP -= 1 * Time.deltaTime;
     }
     
-    private void LineUpdate()
-    { 
-       neighbourNodeTransform = new Transform[neighbourNodes.Length];
-       for (int i = 0; i < neighbourNodes.Length; i++)
-       {
-              neighbourNodeTransform[i] = neighbourNodes[i].transform;
-       }
-       line.SetUpLine(neighbourNodeTransform);
+    private void CreatePaths()
+    {
+
+
+        foreach (BattleNode i in neighbourNodes)
+        {
+            CreatePath(i);
+        }
     }
+
+    public void CreatePath(BattleNode i)
+    {
+        //Variables to create the path at the right length and angle
+        float dist;
+        Vector3 mid;
+        float angle;
+
+        dist = Vector3.Distance(transform.position, i.transform.position);
+        mid = (transform.position + i.transform.position) / 2;
+        angle = Mathf.Atan2(i.transform.position.y - transform.position.y, i.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+
+        //Create the path and stretch and rotate it.
+        GameObject pp = Instantiate(path, mid, Quaternion.identity, /*pathParent.*/transform);
+        pp.transform.localScale = new Vector3(dist, 0.7f, 1);
+        pp.transform.Rotate(0, 0, angle);
+        pp.name = this.name + " to " + i.name;
+    }
+
 
     private void CastleCapture()
     {
+        //Capture the castle and reset its hp
         isEnemy = !isEnemy;
         sr.color = isEnemy ? Color.red : Color.blue; 
         castleCurrHP = castleMaxHP;
+
+        //Let the connected nodes know it has been captured.
+        foreach(BattleNode i in neighbourNodes)
+        {
+            i.CheckSurround();
+        }
+
+        //Boss entities can't be surrounded
+        /*if (isBoss)
+        {
+            GameController.EndStage(true);
+        }*/
+    }
+
+    public void CheckSurround()
+    {
+        bool surround = false;
+        if (neighbourNodes.Count > 1)
+        {
+            Debug.Log(name + " is checking");
+            surround = true;
+            int checkSplit = 0;
+            foreach (BattleNode i in neighbourNodes)
+            {
+                if (i.IsEnemy() == isEnemy)
+                {
+
+                    surround = false;
+                }
+                else
+                {
+                    checkSplit++;
+                }
+            }
+            splitCount = checkSplit;
+
+        }
+        if (surround && !isBoss)
+        {
+            Debug.Log(this.name + "is Surrounded");
+            CastleCapture();
+        }else if(splitCount > 0)
+        {
+            EnemySummonUnits();
+        }
+    }
+
+    public bool IsEnemy()
+    {
+        return isEnemy;
+    }
+
+    public void AddNeighbour(BattleNode neighbour)
+    {
+        if (!neighbourNodes.Contains(neighbour))
+        {
+            neighbourNodes.Add(neighbour);
+        }
+    }
+
+    private void EnemySummonUnits()
+    {
+        StopAllCoroutines();
+        foreach(BattleNode i in neighbourNodes)
+        {
+            if (!i.IsEnemy())
+            {
+                Debug.Log("S" + this.name);
+                StartCoroutine(SummonUnit(testUnit, i.transform));
+            }
+        }
+    }
+
+    private IEnumerator SummonUnit(Unit unit, Transform dest)
+    {
+        while (isEnemy)
+        {
+            yield return new WaitForSeconds(unit.GetSpawnSpeed() * (splitCount*0.75f));
+            unit.SpawnUnit(transform, dest);
+        }
     }
 
 }
